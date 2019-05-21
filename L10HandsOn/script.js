4a1aebe33df5d8d31708af21dae4d3272d381ece
obichidi <39554099+obichidi@users.noreply.github.com>
2019-05-21 15:13:13 -0400

    Add files via upload

---
+++ L10HandsOn/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("social").hover(function(){
+        $(this).addClass("a.social:hover");
+    });
+});
+
+let gitHubRequest = new XMLHttpRequest();
+gitHubRequest.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    let gitObject = JSON.parse(this.responseText);
+    document.getElementById("gitRepo").innerHTML = gitObject.bio;
+  }
+};
+gitHubRequest.open("GET", "https://api.github.com/users/obichidi/repos", true);
+gitHubRequest.send();
